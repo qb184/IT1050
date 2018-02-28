5f309f3711f482f81e61bc8be1cc34d1da5c6534
qnb <QUYNHBUI184@GMAIL.COM>
2018-02-28 00:52:46 -0500

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,67 @@
+﻿using System;
+namespace Project2
+{
+    public class Person
+    {
+        public int age;
+        public string firstName;
+        public string lastName;
+        public Person spouse;
+
+        public static double sumOfAllAges;
+        public static int count;
+
+        public string getFullName()
+        {
+            return this.firstName + " " + this.lastName;
+        }
+     
+        public void printNameAndAge()
+        {
+            Console.WriteLine(this.getFullName() + " (" + this.age + ")");
+        }
+
+        public void askUser()
+        {
+            Console.Write("What is your first name?         ");
+            firstName = Console.ReadLine();
+            Console.Write("What is your last name?          ");
+            lastName = Console.ReadLine();
+            Console.Write("How old are you?                 ");
+            age = int.Parse(Console.ReadLine());
+            count++;
+            sumOfAllAges= sumOfAllAges+ age;
+        }
+
+        public void askSpouse()
+        {
+            Console.Write("Are you married? (Yes/No)        ");
+            string a = Console.ReadLine();
+            if (a == "yes")
+            {
+                Console.Write("What is your spouse's name?      ");
+                spouse.firstName = Console.ReadLine();
+                Console.Write("How old is your spouse?          ");
+                spouse.age = int.Parse(Console.ReadLine());
+                spouse.lastName = lastName;
+                count++;
+                sumOfAllAges= sumOfAllAges+spouse.age;
+            }
+        }
+
+        public static double averageAge()
+        {
+            return Person.sumOfAllAges/ Person.count++;
+        }
+    }
+}
+
+      
+       
+
+    
+
+
+
+
+
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variable Declaration
+            // [type] [name]
+
+            // When you instantiate (create) a new object, it automatically
+            // gets member variables created inside it, which were specified
+            // inside the class.
+
+            Person p1 = new Person(); // Main Program Variable Declaration 1
+            Person p2 = new Person(); // Main Program Variable Declaration 2
+
+            p1.spouse = new Person(); // p1 Member Variable
+            p2.spouse = new Person(); // p2 Member Variable
+
+            Console.WriteLine("Enter Information for p1");
+            Console.WriteLine("\n");
+
+            p1.askUser();
+            p1.askSpouse();
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Enter information for p2");
+            Console.WriteLine("\n");
+
+            p2.askUser();
+            p2.askSpouse();
+
+            Console.WriteLine("\n");
+             
+            p1.printNameAndAge();
+            p1.spouse.printNameAndAge();
+            p2.printNameAndAge();
+            p2.spouse.printNameAndAge();
+
+
+            //Console.WriteLine(Person.sumOfAllAges);
+            //Console.WriteLine(Person.count++);
+            Console.WriteLine("\n");
+            Console.WriteLine("Average age = " + Person.averageAge());
+
+        }
+    }
+}
